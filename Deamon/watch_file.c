f79ac5d5a0dc17dbb08a02aa854bc2be8796becc
IamGatheru <davidgatheru490@gmail.com>
2024-02-23 03:11:54 +0300

    Debugged and added a Makefile

--- Deamon/watch_file.c
+++ Deamon/watch_file.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdbool.h>
 #include <stdint.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/inotify.h>
+#include <stdbool.h>
 
-#include <libnotify/modify.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/inotify.h>
+#include <libnotify/notify.h>
 
 #define EXT_SUCCESS 0
 #define EXT_ERR_TOO_FEW_ARGS 1
@@ -15,100 +16,159 @@
 #define EXT_ERR_ADD_WATCH 3
 #define EXT_ERR_BASE_PATH_NULL 4
 #define EXT_ERR_READ_INOTIFY 5
+#define EXT_ERR_INIT_LIBNOTIFY 6
 
 int IeventQueue = -1;
-int IeventStatus = -1;
+int IeventDescriptor = -1;
+
+char *ProgramTitle = "watch_file";
+
+/* Catch shutdown signals so we can
+ * cleanup exit properly */
+void sig_shutdown_handler(int signal) {
+	int closeStatus;
+
+	printf("Exit signal received!\nclosing inotify descriptors...\n");
+
+	// don't need to check if they're valid because
+	// the signal handlers are registered after they're created;
+	// we can assume they are set up correctly.
+	closeStatus = inotify_rm_watch(IeventQueue, IeventDescriptor);
+	if (closeStatus == -1) {
+		fprintf(stderr, "Error removing file from inotify watch!\n");
+	}
+	close(IeventQueue);
+
+	notify_uninit();
+	exit(EXT_SUCCESS);
+}
 
 int main(int argc, char **argv) {
 
-    char *basePath = NULL;
+    bool notifyInitStatus;
+
+    char buffer[4096];
+
     char *token = NULL;
-    char *notificationMessage = NULL;
+    char *basePath = NULL;
 
-    char buffer[4996];
+	char *notificationMessage = NULL;
 
-    const uint32_t watchMask = IN_CREATE | IN_DELETE | IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF;
+	int readLength = 0;
 
-    int readLength;
+	NotifyNotification *notifyHandle;
 
-    const struct inotify_event* watchEvent;
+	const struct inotify_event* watchEvent;
 
-    if (argc < 2) {
-        fprintf(stderr, "USAGE: watch_file PATH\n");
-        exit(EXT_ERR_TOO_FEW_ARGS);
-    }
+	const uint32_t watch_flags = IN_CREATE | IN_DELETE |
+		IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF;
 
-    basePath = (char *)malloc(sizeof(char)*(strlen(argv[1]) + 1));
-    strcpy(basePath, argv[1]);
+	// argv[1] is the filename to watch
+	if (argc < 2) {
+		fprintf(stderr, "USAGE: %s PATH\n", ProgramTitle);
+		exit(EXT_ERR_TOO_FEW_ARGS);
+	}
 
-    token = strtok(basePath, "/");
-    while (token != NULL) {
-        basePath = token;
-        token = strtok(NULL, "/");
-    }
+	notifyInitStatus = notify_init (ProgramTitle);
 
-    if (basePath == NULL) {
-        fprintf(stderr, "Error getting base path!\n");
-        exit(EXT_ERR_BASE_PATH_NULL);
-    }
+	if (!notifyInitStatus) {
+		fprintf(stderr, "Error initialising with libnotify!\n");
+		exit(EXT_ERR_INIT_LIBNOTIFY);
+	}
 
-    IeventQueue = inotify_init();
-    if (IeventQueue == -1) {
-        fprintf(stderr,"Error initialising inotify instance\n");
-        exit(EXT_ERR_INIT_INOTIFY);
-    }
+	IeventQueue = inotify_init();
 
-    IeventStatus = inotify_add_watch(IeventQueue, argv[1], watchMask);
-    if (IeventStatus == -1) {
-        fprintf(stderr, "Error adding file to watch instance!\n");
-        exit(EXT_ERR_ADD_WATCH);
-    }
-//Build persistence and since a deamo should run forever, we must comment out exit statement and introduce a while true loop
+	if (IeventQueue == -1) {
+		fprintf(stderr, "Error initialising event queue!\n");
+		exit(EXT_ERR_INIT_INOTIFY);
+	}
 
-while (true){
-    printf("Waiting for ievent...\n");
+	IeventDescriptor = inotify_add_watch(IeventQueue, argv[1], watch_flags);
 
-    readLength = read(IeventQueue, buffer, sizeof(buffer));
-    if (readLength == -1) {
-        fprintf(stderr, "Error reading from inotify instance\n");
-        exit(EXT_ERR_READ_INOTIFY);
-    }
+	if (IeventDescriptor == -1) {
+		fprintf(stderr, "Error adding file to inotify watch!\n");
+		exit(EXT_ERR_ADD_WATCH);
+	}
 
-    for (char *bufferPointer = buffer; bufferPointer < buffer + readLength; bufferPointer += sizeof(struct inotify_event) + watchEvent->len) {
-        notificationMessage = NULL;
-        watchEvent = (const struct inotify_event *)bufferPointer;
+	// register signal handlers, note we
+	// have already checked all error states
+	signal(SIGABRT, sig_shutdown_handler);
+	signal(SIGINT, sig_shutdown_handler);
+	signal(SIGTERM, sig_shutdown_handler);
 
+	// strtok is destructive so make
+	// a copy for it to use instead
+	basePath = (char *)malloc(sizeof(char)*(strlen(argv[1]) + 1));
+	strcpy(basePath, argv[1]);
 
-        if (watchEvent->mask & IN_CREATE) {
-            notificationMessage = "File created.\n";
-        }
+	token = strtok(basePath, "/");
+	while (token != NULL) {
+		basePath = token;
+		token = strtok(NULL, "/");
+	}
 
-        if (watchEvent->mask & IN_DELETE) {
-            notificationMessage = "File deleted.\n";
-        }
-        if (watchEvent->mask & IN_ACCESS) {
-            notificationMessage = "File accessed.\n";
-        }
-        if (watchEvent->mask & IN_CLOSE_WRITE) {
-            notificationMessage = "File written and closed.\n";
-        }
-        if (watchEvent->mask & IN_MODIFY) {
-            notificationMessage = "File modified.\n";
-        }
-        if (watchEvent-> mask & IN_MOVE_SELF) {
-            notificationMessage = "File moved.\n";
-        }
+	if (basePath == NULL) {
+		fprintf(stderr, "Error getting base file path!\n");
+		exit(EXT_ERR_BASE_PATH_NULL);
+	}
 
-        if (notificationMessage == NULL) {
-            continue;
-        }
-        printf("%s\n", basePath);
+	// daemon main loop
+	while (true) {
 
-    }
+		// block on inotify event read
+		printf("Waiting for ievent...\n");
+		readLength = read(IeventQueue, buffer, sizeof(buffer));
 
-}
+		if (readLength == -1) {
+			fprintf(stderr, "Error reading from inotify event!\n");
+			exit(EXT_ERR_READ_INOTIFY);
+		}
 
+		// one read could yield multiple events; loop through all
+		// kinda tricky to get at a glance but basically moves the pointer
+		// through the buffer according to the length of the inotify_event struct
+		// and the length of the previous inotify event data
+		for (char *buffPointer = buffer; buffPointer < buffer + readLength;
+			 buffPointer += sizeof(struct inotify_event) + watchEvent->len) {
 
+			notificationMessage = NULL;
+			watchEvent = (const struct inotify_event *) buffPointer;
 
-//exit(EXIT_SUCCESS);
+			if (watchEvent->mask & IN_CREATE) {
+				notificationMessage = "File created.\n";
+			}
+
+			if (watchEvent->mask & IN_DELETE) {
+				notificationMessage = "File deleted.\n";
+			}
+
+			if (watchEvent->mask & IN_ACCESS) {
+				notificationMessage = "File accessed.\n";
+			}
+
+			if (watchEvent->mask & IN_CLOSE_WRITE) {
+				notificationMessage = "File written and closed.\n";
+			}
+
+			if (watchEvent->mask & IN_MODIFY) {
+				notificationMessage = "File modified.\n";
+			}
+
+			if (watchEvent->mask & IN_MOVE_SELF) {
+				notificationMessage = "File moved.\n";
+			}
+
+			if (notificationMessage == NULL) {
+				continue;
+			}
+
+			notifyHandle = notify_notification_new(basePath, notificationMessage, "dialog-information");
+			if (notifyHandle == NULL) {
+				fprintf(stderr, "Got a null notify handle!\n");
+				continue;
+			}
+			notify_notification_set_urgency(notifyHandle, NOTIFY_URGENCY_CRITICAL);
+			notify_notification_show(notifyHandle, NULL);
+		}
+	}
 }
